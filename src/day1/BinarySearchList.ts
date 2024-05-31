69493bd12543c3303a2d9b259919f3dc6213af2c
Serhat Can <serhatcan1996@gmail.com>
2024-05-31 21:22:21 +0200

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    while (low < high) {
+        let mid = Math.floor((high + low) / 2);
+        let val = haystack[mid];
+
+        if (val === needle) {
+            return true;
+        } else if (val > needle) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return false;
 }
