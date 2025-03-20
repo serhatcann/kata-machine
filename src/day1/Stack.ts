a4fd885d7f157e792ce6605cb90e307e98669d9c
Serhat Can <serhatcan1996@gmail.com>
2025-03-20 23:14:20 +0100

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node: Node<T> = { value: item };
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.next = this.head;
+        this.head = node;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free
+        head.next = undefined;
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
