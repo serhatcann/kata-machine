232164c1852962ca34742d39d2fed35b3963b8aa
Serhat Can <serhatcan1996@gmail.com>
2025-04-12 21:11:55 +0200

    breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,17 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = [head];
 
+    while (queue.length > 0) {
+        const curr = queue.shift();
+
+        if (!curr) {
+            continue;
+        }
+        if (curr.value === needle) return true;
+
+        if (curr.left) queue.push(curr.left);
+        if (curr.right) queue.push(curr.right);
+    }
+
+    return false;
 }
