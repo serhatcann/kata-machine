3b41f9673f38d7f52a3c5d2273ed7c76518700bd
Serhat Can <serhatcan1996@gmail.com>
2025-04-13 15:20:12 +0200

    dfs on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,32 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    if (!head) return false;
+    if (head.value === needle) return true;
+    if (head.left && head.value >= needle) {
+        return dfs(head.left, needle);
+    } else if (head.right && head.value <= needle) {
+        return dfs(head.right, needle);
+    }
 
+    return false;
 }
+
+// iterative
+// export default function dfs(
+//     head: BinaryNode<number> | null,
+//     needle: number,
+// ): boolean {
+//     if (!head) return false;
+
+//     while (head !== null) {
+//         if (head.value === needle) {
+//             return true;
+//         }
+//         if (head.value >= needle) {
+//             head = head.left;
+//         } else {
+//             head = head.right;
+//         }
+//     }
+
+//     return false;
+// }
